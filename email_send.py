0851db1823271bb33ed2e32b66ae362c9a79056a
Sivasankara Eswari S M <kala.sankari199@gmail.com>
2024-10-02 16:49:12 +0530

    Update email_send.py

--- email_send.py
+++ email_send.py
@@ -5,11 +5,11 @@ to = input("Enter the email address of the receiver:")
 message = input("Enter the message: ")
 
 def sendEamil(to, message):
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login('senderemail', 'password')
-server.sendmail(senderemail, to, message) 
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('senderemail', 'password')
+    server.sendmail(senderemail, to, message) 
 
-server.close
+    server.close
 
 sendEamil(to, message)
