6dad737a3096ff2e264c04232a160c7dae6a11ee
Sivasankara Eswari S M <kala.sankari199@gmail.com>
2024-10-02 14:13:44 +0530

    Update email_send.py

--- email_send.py
+++ email_send.py
@@ -5,6 +5,6 @@ server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login('senderemail', 'password')
 server.sendmail (senderemail, to, testing email automation!') 
-server.close()
+server.close
 
 sendEamil()
